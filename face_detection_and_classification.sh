221317dbefb9da9365f9344d2e7960f47f8694d7
jamin.jeong <mooneclipse03@gmail.com>
2020-03-06 20:58:05 +0900

    chagned data path

--- face_detection_and_classification.sh
+++ face_detection_and_classification.sh
@@ -1,6 +1,3 @@
 python face_detection_and_classification.py \
-  -i "/home/jamin/micube/projects/Code/face/tensorflow-face-detection/media/test.mp4" \
-  -f "/home/jamin/micube/projects/Code/face/tensorflow-face-detection/media/face_image" \
-
-#  -i "./face_detection/tensorflow-face-detection/media/test.mp4" \
-#  -f "./face_detection/tensorflow-face-detection/media/face_image" \
+  -i "./face_detection/tensorflow-face-detection/media/test.mp4" \
+  -f "./face_detection/tensorflow-face-detection/media/face_image" \
